ddd1cff9080a31757152e354c214b9de1ed8b662
Mohammad Umer <33020678+imumar@users.noreply.github.com>
2020-04-10 10:33:36 +0530

    Update database.php

--- database.php
+++ database.php
@@ -2,6 +2,6 @@
 $con = mysqli_connect("localhost","phpmyadmin","toor","register");
 if (mysqli_connect_errno())
   {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  echo "Failed to connect to database: " . mysqli_connect_error();
   }
 ?>
